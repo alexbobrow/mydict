2ed8f1ebe584026a80a4526e0e06b1123bb774eb
Alex <alexbobrow@mail.ru>
2016-07-22 21:44:00 +0000

    First unit test and fixture

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Word
+
+
+class AnimalTestCase(TestCase):
+
+    fixtures = ['words2000.json']
+
+    def test_initial_words_count(self):
+        count = Word.objects.all().count()
+        self.assertEqual(count, 0)
+
+
