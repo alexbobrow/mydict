a4c5c73c4be80cad3867bde0e6568a274dc8b02b
Alex <alexbobrow@mail.ru>
2016-08-07 11:10:52 +0000

    Sending answer if only one variant in autocomplete the popup Getting next word after repeated Enter

--- main/static/app.js
+++ main/static/app.js
@@ -1,8 +1,27 @@
 $(function(){
 
+    var currentProgressId = null;
 
+    var csrf = getCookie('csrftoken');
+
+    var ANSWERING = 0;
+    var RESULT = 1;
+    var PROCESSING = 3;
+
+    var status = null;
 
 	$(function(){
+
+
+        $('input.test').on('keyup', function(e){
+            var code = (e.charCode)? e.charCode : e.keyCode;
+            if (status==RESULT && code==13) {
+                $('input.test').val('');
+                next();
+                console.log('Enterok!');
+            }
+        });
+
 
         alexSuggest('input.test', {
             url: '/api/suggest',
@@ -15,6 +34,14 @@ $(function(){
                 var id = e.attr('data-id');
                 sendAnswer(id);
             },
+            onPlainEnter: function(i){
+                var o = i.data('alexSuggest');
+                if ($('#alsuli' + " li").length==1) {
+                    var id = $('#alsuli' + " li").attr('data-id');
+                    sendAnswer(id);
+                }
+
+            }
         });
 
 
@@ -22,21 +49,28 @@ $(function(){
 
 	});
 
-    var currentProgressId = null;
 
-    var csrf = getCookie('csrftoken');
 
 
     function next() {
+        if (status==PROCESSING || status==ANSWERING) {
+            return false;
+        }
+        status = PROCESSING;
+        $('input.test').removeClass('wrong').removeClass('correct');
+        $('.answer').fadeOut();
         $.get('/api/next', {}, function(ans){
             $('.word').text(ans.word);
             currentProgressId = ans.id;
+            status = ANSWERING;
         }, 'json');
     }
 
 
 
     function sendAnswer(id) {
+
+        status = PROCESSING;
 
         var data = {
             answer_id: id,
@@ -45,12 +79,13 @@ $(function(){
         }
 
         $.post('/api/answer', data, function(ans){
-            $('.answer').text(ans.answer);
+            $('.answer').text(ans.answer).fadeIn();
             if (ans.correct) {
                 $('input.test').addClass('correct');
             } else {
                 $('input.test').addClass('wrong');
             }
+            status = RESULT;
         }, 'json');
 
 
