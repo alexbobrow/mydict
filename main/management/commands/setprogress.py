ddf4691907200766c4dcc9fe3d9fdb15e1904f6d
Alex <alexbobrow@mail.ru>
2017-02-11 05:49:17 +0000

    Setprogress management command improved, now it updates rating of old words

--- main/management/commands/setprogress.py
+++ main/management/commands/setprogress.py
@@ -28,13 +28,31 @@ class Command(BaseCommand):
 
         need_to_add = int(total) - current_count
 
+
+        self.stdout.write('User has %s word(s), need to add %s' % (current_count, need_to_add))
+
+        self.stdout.write('Updating rating for old words...')
+
+
+        for progress in user.progress_set.all():
+            # asked
+            a = random.randint(asked-2, asked+2)
+            # ratio
+            r = random.randint(rating100-20, rating100+20)
+            # correct
+            #import pdb; pdb.set_trace()
+            c = int(round(float(r) / 100 * a))
+
+            progress.asked = a
+            progress.correct_answers = c
+            progress.save()
+
+
         if need_to_add<=0:
             self.stdout.write('Cancelation. User already has %s word(s)' % (current_count))
             return
 
-        self.stdout.write('User has %s word(s), need to add %s' % (current_count, need_to_add))
-
-        self.stdout.write('Working...')
+        self.stdout.write('Adding new words...')
 
         #addNewWordBulk(self, user, count):
         for x in range(0, need_to_add):
