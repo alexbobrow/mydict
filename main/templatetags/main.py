90c40328e48594bbb24b6ff543ba24a9e84d6797
Alex <alexbobrow@mail.ru>
2017-03-16 10:56:44 +0000

    User id showed if user lacks both first and last name

--- main/templatetags/main.py
+++ main/templatetags/main.py
@@ -8,3 +8,13 @@ def pagelink(request, value):
     d = request.GET.copy()
     d['p'] = value
     return "?%s" % d.urlencode()
+
+
+@register.filter
+def sensible(user):
+	fn = user.get_full_name()
+	if not fn:
+		return "Пользователь №%s" % user.pk
+	else:
+		return fn
+
