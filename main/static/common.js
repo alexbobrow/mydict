4f1282431afcdab4cf17f76069590b3b3f24fa5d
Alex Bobrov <alexbobrow@mail.ru>
2017-04-17 23:59:35 +0500

    Menu toggle button UX improved

--- main/static/common.js
+++ main/static/common.js
@@ -85,13 +85,8 @@
     function listeners() {
 
 
-        $('body').on('click', 'button.open-menu', function(){
-            $('div.body').addClass('desktop-menu');
-        });
-
-
-        $('body').on('click', 'button[data-action=close-menu]', function(){
-            $('div.body').removeClass('desktop-menu');
+        $('body').on('click', 'button.toggle-menu', function(){
+            $('div.body').toggleClass('menu-opened');
         });
 
 
