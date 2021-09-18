1b577ac80a934503a816d7e51efa567d7b02ec5c
Alexandr Bobrov <alexbobrow@mail.ru>
2021-09-18 18:46:00 +0500

    Change urlpatterns to path

--- main/urls.py
+++ main/urls.py
@@ -1,15 +1,16 @@
-from django.conf.urls import url
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.root),
-    url(r'^logout$', views.logout_view, name='logout'),
-    url(r'^cards/$', views.cards, name='cards'),
-    url(r'^list/$', views.list, name='list'),
-    url(r'^api/next$', views.next, name='next'),
-    url(r'^api/report-word$', views.report_word, name='report'),
-    url(r'^api/delete-word$', views.delete_word, name='delete'),
-    url(r'^api/admin-update-word$', views.admin_update_word, name='admin_update'),
-    url(r'^api/user-prefs/$', views.user_prefs, name='user_prefs'),
-    url(r'^admin/stata/$', views.stata, name='stata'),
+    path('', views.root),
+    path('logout', views.logout_view, name='logout'),
+    path('cards/', views.cards, name='cards'),
+    path('list/', views.list, name='list'),
+    path('api/next', views.next, name='next'),
+    path('api/report-word', views.report_word, name='report'),
+    path('api/delete-word', views.delete_word, name='delete'),
+    path('api/admin-update-word', views.admin_update_word, name='admin_update'),
+    path('api/user-prefs/', views.user_prefs, name='user_prefs'),
+    path('admin/stata/', views.stata, name='stata'),
 ]
